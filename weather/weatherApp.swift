3d0d796bc046cd36e59a606a087d7efa45422eda
Ilya Kolbeko <kolbeko@yahoo.com>
2022-02-16 23:07:05 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Илья Колбеко on 16.02.22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
